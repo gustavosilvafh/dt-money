8dc8e6ff6ff2e342f518e790985b91f6a2c8743d
gustavosilvaf <gustavofsilva@email.com>
2021-07-16 13:00:28 -0300

    configuring mirage

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  useEffect(() => {
+    fetch("http://localhost:3000/api/transactions")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento Site</td>
+            <td className="deposit">R$1000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$1000,00</td>
+            <td>Casa</td>
+            <td>17/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
